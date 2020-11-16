a5785d45f68207d77f87ffc6dd38fca5416d95b5
Mitra Nami <a.mxnami@gmail.com>
2020-11-16 11:28:15 -0500

    Change the button from start the course to continue the course when registered

--- client/src/components/CourseHomePage.js
+++ client/src/components/CourseHomePage.js
@@ -7,35 +7,35 @@ const CourseHomePage = (props) => {
   const { courseId } = useParams();
   const {registrations, registerUser} = useRegistrationData();
 
+  //if the user is not logged(id null) or not registered in the course, is Registered will be false
+  //if the user is logged in and registered it would be true
+  const isRegistered = isRegisteredForACourse(props.state.token ? props.state.token.userId : 0, courseId, registrations);
+
   const handleClick = () => {
     if (props.state.token) {
       //the user is logged in, so we need to see if they are registered in this course
-      
-      if (isRegisteredForACourse(props.state.token.userId, courseId, registrations)) {
-        console.log('user is registerd for the course, take them to the content page ');
+    
+      //you need to register them in the course, there are three possibilities
+      const course = getCourseById(courseId, props.state.courses);
+      console.log(course);
+
+      if (course.authorized === true && course.price === 0) {
+        console.log('just register them')
+
+        registerUser(props.state.token.userId, courseId)
+          .then(() => console.log('take them to the content page'))
+          .catch(error => console.log('didn not registered successfully'))
+
+
         
-      } else {
-        //you need to register them in the course, there are three possibilities
-        const course = getCourseById(courseId, props.state.courses);
-        console.log(course);
+      } else if (course.authorized === true && course.price !== 0) {
+        console.log('lead the to pyament page');
+        //on successful payment, reister them, take them to contenct page
+      } else if (course.authorized === false) {
+        console.log('send a request msg to admin to be enrolled');
 
-        if (course.authorized === true && course.price === 0) {
-          console.log('just register them')
-
-          registerUser(props.state.token.userId, courseId)
-            .then((result) => console.log(result))
-            .catch()
-
-
-          //take them to content page
-        } else if (course.authorized === true && course.price !== 0) {
-          console.log('lead the to pament page');
-          //on successful payment, reister them, take them to contenct page
-        } else if (course.authorized === false) {
-          console.log('send a request msg to admin to be enrolled');
-
-        }
       }
+    
 
     } else {
       //the user is not logged in, show them signup/login modal
@@ -47,7 +47,8 @@ const CourseHomePage = (props) => {
   return(
     <div>
       Home page of course with Id: {courseId}
-      <button type="submit" className="btn btn-primary" onClick={handleClick}>Start the Course!</button>
+      {!isRegistered && <button type="submit" className="btn btn-primary" onClick={handleClick}>Start the Course!</button>}
+      {isRegistered && <button type="submit" className="btn btn-primary" onClick={() => console.log('taken to the content page')}>Continue the Course!</button>}
     </div>
   )
 };
