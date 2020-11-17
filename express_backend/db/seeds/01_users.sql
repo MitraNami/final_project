5eeed49d543405590d3bd1096452d166149ef178
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-11-17 12:55:28 -0500

    Update user passwords in the DB

--- express_backend/db/seeds/01_users.sql
+++ express_backend/db/seeds/01_users.sql
@@ -1,13 +1,13 @@
 INSERT INTO
 users(first_name, last_name, email, password, type, avatar_url)
 VALUES
-    ('Mario', 'Bros', 'mario@nintendo.com', 'test', 'admin', 'client/public/person-male.png'),
-    ('Luigi', 'Bros', 'luigi@nintendo.com', 'test', 'client', 'client/public/person-male.png'),
+    ('Mario', 'Bros', 'mario@nintendo.com', '$2b$10$8XpRtCvUSRqsyI3NsvlZZ.1aw21koHFGeGv3tvHru5xNB.hNLn4lK', 'admin', 'client/public/person-male.png'),
+    ('Luigi', 'Bros', 'luigi@nintendo.com', '$2b$10$8XpRtCvUSRqsyI3NsvlZZ.1aw21koHFGeGv3tvHru5xNB.hNLn4lK', 'client', 'client/public/person-male.png'),
     (
         'Princess',
         'Peach',
         'peach@nintendo.com',
-        'test',
+        '$2b$10$8XpRtCvUSRqsyI3NsvlZZ.1aw21koHFGeGv3tvHru5xNB.hNLn4lK',
         'client', 
         'client/public/person-female.png'
     ),
@@ -15,8 +15,9 @@ VALUES
         'Princess',
         'Daisy',
         'daisy@nintendo.com',
-        'test',
+        '$2b$10$8XpRtCvUSRqsyI3NsvlZZ.1aw21koHFGeGv3tvHru5xNB.hNLn4lK',
         'client', 
         'client/public/person-female.png'
     ),
-    ('Donkey', 'Kong', 'donkey@nintendo.com', 'test', 'client', 'client/public/person-male.png');
+    ('Donkey', 'Kong', 'donkey@nintendo.com', '$2b$10$RDXaiWph46XHKKhtiAOpseQJxHqjTkBaHszycWM6pvW0A5SEPP2ke', 'client', 'client/public/person-male.png'),
+    ('Bia', 'Bia', 'bia@g.com', '$2b$10$8XpRtCvUSRqsyI3NsvlZZ.1aw21koHFGeGv3tvHru5xNB.hNLn4lK', 'admin', 'client/public/person-female.png');
