85c533c90cb8cba52c1947c19b156e38e95e7939
Mitra Nami <a.mxnami@gmail.com>
2020-11-18 11:37:49 -0500

    Include the course id as a parameter in the route

--- express_backend/routes/lessons.js
+++ express_backend/routes/lessons.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = () => {
+module.exports = ({
+  getLessonsByCourseId
+}) => {
+
   router.get('/course/:id', (req, res) => {
     //get the lessons with the given course id
-    getLessons()
+    const courseId = req.params.id;
+    getLessonsByCourseId(Number(courseId))
       .then(lessons => res.json(lessons))
       .catch((err) => res.json({
         error: err.message
