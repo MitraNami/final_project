999be35ccd001ced173d4cd92b71b1d1f600b9ad
Mitra Nami <a.mxnami@gmail.com>
2020-11-18 18:03:17 -0500

    Add fake url for videos and notes

--- express_backend/db/seeds/03_lessons.sql
+++ express_backend/db/seeds/03_lessons.sql
@@ -5,23 +5,23 @@ VALUES
         'Lesson 1',
         'Enroll in this free course to learn more about the StrongerU Senior Fitness Instructor Certification and Series. We will review the steps to becoming a StrongerU Senior Fitness Instructor, show you a preview of the choreography notes and instructional videos for the four StrongerU Senior Fitness Series formats- Cardio, Strength, Stretch, and Circuit, and more.',
         to_timestamp(1605575289),
-        'http://www.example.com',
-        'http://www.example.com',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108731/samples/elephants.mp4',
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         1
     ),
     (
         'Lesson 2',
         'The StrongerU Senior Fitness Instructor Certification is a premier first pre-choreographed senior fitness certification. Instructors complete this course at their own pace and from any device including a smartphone. At the end of the course, you will receive your first 30-minutes of choreography via an instructional video and choreography notes. The final step to becoming a fully certified StrongerU Senior Fitness Instructor is to submit a practical video of you teaching the choreography and showing you meet the program s core competencies. Upon receiving a passing grade on your practical video, you are now eligible to subscribe to the StrongerU Senior Fitness Series where each month you will receive 30-minutes of content in a rotation of Cardio, Strength, Stretch and Circuit. Please read our terms of services before completing your registration. By registering, you are accepting these terms of service.',
         to_timestamp(1605575289),
-        'http://www.example.com',
-        'http://www.example.com',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         1
     ),
     (
         'Lesson 3',
         'Subscription to the StrongerU Senior Fitness Series provides StrongerU Senior Fitness Instructors with their licence to teach the StrongerU Senior Fitness program. Each month you will receive 30-minutes of new content, including; an instructional video and choreography notes.',
         to_timestamp(1605575289),
-        'http://www.example.com',
-        'http://www.example.com',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         1
     );
