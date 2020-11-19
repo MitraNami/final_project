0c4dd50bc4513ef8eaa2525281d949bc03acd68e
Mitra Nami <a.mxnami@gmail.com>
2020-11-19 13:35:16 -0500

    Show all the lessons of course that doesn't require subscription

--- client/src/components/CoursePage.js
+++ client/src/components/CoursePage.js
@@ -9,26 +9,35 @@ const CoursePage = (props) => {
   const { courseId } = useParams();
 
   const course = getCourseById(courseId, props.state.courses);
-  const subscriptionBased = course.subscription_based;
-  console.log(subscriptionBased)
-
+  const subscriptionBased = course && course.subscription_based;
+  
    //we need to get all the lessons with this course id from the database
   const {lessons} = useContentData(courseId);
   console.log(lessons);
 
-  
- 
-
-    //you needd to go to the lessons table and get all the lessons
-    //for this course id; then show them all. if the course is
+    //if the course is
     //not subscription based then give access to all of them
+
+    let LessonsList = [];
+    if (!subscriptionBased) {
+      LessonsList = lessons.map(lesson => <UserLesson key={lesson.id} lesson={lesson} access={true}/>)
+    } else {
+      LessonsList = lessons.map(lesson => {
+        const lessonReleaseDate = lesson.release_date;
+        console.log(lessonReleaseDate)
+        return <UserLesson key={lesson.id} lesson={lesson} access={false} />;
+      });
+    }
+
+
+
     //if subscription based only give access to the ones in
     //the sub period. lock the rest with the buy now button
   return (
     <div>
       Content of course with id: {courseId}
 
-      <UserLesson />
+      {LessonsList}
       
 
 
