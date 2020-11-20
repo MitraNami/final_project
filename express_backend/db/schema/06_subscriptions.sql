a0acaea4a1a8caa8988c2b1854473753290b0e77
Mitra Nami <a.mxnami@gmail.com>
2020-11-19 20:00:52 -0500

    Make null the default value of end_date since javascript doesnt give back infinity

--- express_backend/db/schema/06_subscriptions.sql
+++ express_backend/db/schema/06_subscriptions.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS subscriptions CASCADE;
 CREATE TABLE subscriptions(
     id SERIAL PRIMARY KEY NOT NULL,
     start_date TIMESTAMP DEFAULT NOW(),
-    end_date TIMESTAMP DEFAULT 'infinity',
+    end_date TIMESTAMP DEFAULT NULL,
     user_id INTEGER REFERENCES users(id),
     course_id INTEGER REFERENCES courses(id)
 );
