6d4daca9e369a91fc75b8dd99a027b52a6b6f59b
Mitra Nami <a.mxnami@gmail.com>
2020-11-19 19:59:26 -0500

    Give access only to the lessons in subscription periods

--- client/src/components/CoursePage.js
+++ client/src/components/CoursePage.js
@@ -3,54 +3,74 @@ import axios from 'axios';
 
 import useContentData from 'hooks/useContentData';
 import UserLesson from 'components/UserLesson';
-import { getCourseById } from '../helpers/selectors';
+import { getCourseById, isActiveSubscriber } from '../helpers/selectors';
+import useSubscriptionData from 'hooks/useSubscriptionData';
 
 const CoursePage = (props) => {
 
   const { courseId } = useParams();
+  //we need to get all the lessons with this course id from the database
+  const {lessons} = useContentData(courseId);
+  //we need to get all the subscriptions for the given useId and courseId
+  const {subscriptions, setSubscription} = useSubscriptionData(props.state.token.userId, courseId);
 
   const course = getCourseById(courseId, props.state.courses);
   const subscriptionBased = course && course.subscription_based;
-  
-   //we need to get all the lessons with this course id from the database
-  const {lessons} = useContentData(courseId);
-  console.log(lessons);
 
-    //if the course is
-    //not subscription based then give access to all of them
+  console.log(subscriptions);
 
     let LessonsList = [];
+    let isSubscriptionActive
+
     if (!subscriptionBased) {
+      //if the course is not subscription based then give the user access to all the lessons
       LessonsList = lessons.map(lesson => <UserLesson key={lesson.id} lesson={lesson} access={true}/>)
     } else {
+      //if the course is subscription based
+      //we need to check if the user has an active subscription for it or not
+      //if they have active subscription, show them the 'cancel subscription' button instead of
+      //the 'subscribe' button
+      isSubscriptionActive = isActiveSubscriber(subscriptions);
+      // we need to give the user access only to the lessons in their subscription periods
+      //Any lesson outside subscription periods must be locked with a 'buy now' button to
+      //purchase the item
       LessonsList = lessons.map(lesson => {
         const lessonReleaseDate = lesson.release_date;
-        console.log(lessonReleaseDate)
+        for (const subscription of subscriptions) {
+          console.log(subscription.start_date, subscription.end_date, lessonReleaseDate)
+          if (subscription.start_date < lessonReleaseDate && 
+            (lessonReleaseDate < subscription.end_date || subscription.end_date === null)) {
+              return <UserLesson key={lesson.id} lesson={lesson} access={true} />
+            }
+        }
         return <UserLesson key={lesson.id} lesson={lesson} access={false} />;
       });
+
     }
 
-
+    
   const handleSubscription = () => {
     axios.post('/api/subscriptions', {
       user_id: props.state.token.userId,
       course_id: courseId
     })
-    .then(result => console.log(result.data))
+    .then(result => {
+      const newSubscription = result.data;
+      setSubscription(newSubscription);
+    })
     .catch(error => console.log(error, 'didn not subscribe successfully'));
 
   };
 
-    //if subscription based only give access to the ones in
-    //the sub period. lock the rest with the buy now button
+  
   return (
     <div>
       Content of course with id: {courseId}
 
-
+      {/* we should see one of these buttons if the couse is subscription based */}
       {subscriptionBased && <>
-        <button onClick={handleSubscription}>Subscribe!</button>
-        <button>Cancel subscription!</button>
+        {!isSubscriptionActive && <button onClick={handleSubscription}>Subscribe!</button>}
+        {isSubscriptionActive && <button>Cancel subscription!</button>}
       </>}
 
       {LessonsList}
