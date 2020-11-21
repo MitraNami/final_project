08556a06ccb5a7ec146266791694ee5dca402a62
Mitra Nami <a.mxnami@gmail.com>
2020-11-20 19:03:58 -0500

    Add a route for subscriptions which updates the end of a subscription

--- express_backend/routes/subscriptions.js
+++ express_backend/routes/subscriptions.js
@@ -6,13 +6,14 @@ const router = express.Router();
 
 module.exports = ({
   addSubscription,
-  getSubscriptionsByUserIdCourseId
+  getSubscriptionsByUserIdCourseId,
+  endSubscription
 }) => {
 
     /*Get subscriptions for a specific user in a specific course */
     router.get('/:userId&:courseId', (req, res) => {
       const {userId, courseId} = req.params;
-      getSubscriptionsByUserIdCourseId(userId, courseId)
+      getSubscriptionsByUserIdCourseId(Number(userId), Number(courseId))
         .then(subsctiptions => res.json(subsctiptions))
         .catch((err) => res.json({
           error: err.message
@@ -35,6 +36,16 @@ module.exports = ({
         error: err.message
       }));
 
+  });
+
+  /*End the subscription for a given subscription id */
+  router.put('/:id', (req, res) => {
+    const id = Number(req.params.id);
+    endSubscription(id)
+      .then(updatedSubsctiption => res.json(updatedSubsctiption))
+      .catch((err) => res.json({
+        error: err.message
+      }));
   });
 
 
