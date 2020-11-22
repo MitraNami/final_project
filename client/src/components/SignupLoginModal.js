119acf465f03e8e26034874e25ab19548f133932
Mitra Nami <a.mxnami@gmail.com>
2020-11-22 16:54:42 -0500

    Add style to the modal

--- client/src/components/SignupLoginModal.js
+++ client/src/components/SignupLoginModal.js
@@ -4,9 +4,7 @@ import Modal from 'react-modal';
 Modal.setAppElement('#root');
 
 const SignupLoginModal = (props) => {
-
   const history = useHistory();
-
   const {modalIsOpen, setModalIsOpen} = props;
 
   const handleLogin = () => {
@@ -20,14 +18,36 @@ const SignupLoginModal = (props) => {
 
   return (
     
-      <Modal isOpen={modalIsOpen}>
-
+      <Modal 
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={
+          {
+            overlay: {
+              backgroundColor: '#D3D3D3E6'
+            },
+            content: {
+              color: 'black',
+              top: '20%',
+              left: '35%',
+              width: '30%',
+              height: '40%',
+              backgroundColor: '#ff99cc',
+              border: 'black solid 3px',
+              borderRadius: '15px 50px 30px'
+            }
+          }
+        }
+        >
         <h4>You need to login or signup first:</h4>
-        <button type="submit" onClick={handleLogin}>Login</button>
         <br />
-        <button type="submit" onClick={handleSignup}>Signup</button>
-        <br />
-        <button type="submit" onClick={() => setModalIsOpen(false)}>Cancel</button>
+        <div className="container d-flex flex-column">
+          <button className="btn btn-outline-dark btn-lg" type="submit" onClick={handleLogin}>Login</button>
+          <br />
+          <button className="btn btn-outline-dark btn-lg" type="submit" onClick={handleSignup}>Signup</button>
+          <br />
+          <button className="btn btn-outline-dark btn-lg" type="submit" onClick={() => setModalIsOpen(false)}>Cancel</button>
+        </div>
       
       </Modal>
     
