7143dc6c766bf961b1a0ab3dc727a440e1349ced
Mitra Nami <a.mxnami@gmail.com>
2020-11-22 12:40:55 -0500

    Add local time to lessons

--- express_backend/db/seeds/03_lessons.sql
+++ express_backend/db/seeds/03_lessons.sql
@@ -13,7 +13,7 @@ VALUES
     (
         'Lesson 2',
         'The StrongerU Senior Fitness Instructor Certification is a premier first pre-choreographed senior fitness certification. Instructors complete this course at their own pace and from any device including a smartphone. At the end of the course, you will receive your first 30-minutes of choreography via an instructional video and choreography notes. The final step to becoming a fully certified StrongerU Senior Fitness Instructor is to submit a practical video of you teaching the choreography and showing you meet the program s core competencies. Upon receiving a passing grade on your practical video, you are now eligible to subscribe to the StrongerU Senior Fitness Series where each month you will receive 30-minutes of content in a rotation of Cardio, Strength, Stretch and Circuit. Please read our terms of services before completing your registration. By registering, you are accepting these terms of service.',
-        to_timestamp(1605575289),
+        to_timestamp(1552572000),
         'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         345,
@@ -22,7 +22,7 @@ VALUES
     (
         'Lesson 3',
         'Subscription to the StrongerU Senior Fitness Series provides StrongerU Senior Fitness Instructors with their licence to teach the StrongerU Senior Fitness program. Each month you will receive 30-minutes of new content, including; an instructional video and choreography notes.',
-        to_timestamp(1605575289),
+        to_timestamp(1550153400),
         'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         877,
@@ -31,7 +31,7 @@ VALUES
     (
         'Lesson 1: what is a semiconductor? ',
         'semiconductor lies between metal (conductors) and non-metal (insulators) therefore it behaves like an insulator and doesnt conduct electricity but depending upon the situation it can also conduct electricity and behave like an insulator.',
-        to_timestamp(1605575289),
+        to_timestamp(1550153400),
         'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
@@ -40,7 +40,7 @@ VALUES
     (
         'Lesson 2: examples of semiconductors ',
         'The study of semiconductor materials began in the early 19th century. The elemental semiconductors are those composed of single species of atoms, such as silicon (Si), germanium (Ge), and tin (Sn) in column IV and selenium (Se) and tellurium (Te) in column VI of the periodic table.',
-        to_timestamp(1605575289),
+        to_timestamp(1552572000),
         'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
@@ -49,7 +49,7 @@ VALUES
      (
         'Lesson 3: semiconductors in industry',
         'The study of semiconductor materials began in the early 19th century. The elemental semiconductors are those composed of single species of atoms, such as silicon (Si), germanium (Ge), and tin (Sn) in column IV and selenium (Se) and tellurium (Te) in column VI of the periodic table.',
-        to_timestamp(1605575289),
+        to_timestamp(1555250400),
         'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
@@ -58,7 +58,7 @@ VALUES
     (
         'Lesson 4: semiconductors in industry',
         'The study of semiconductor materials began in the early 19th century. The elemental semiconductors are those composed of single species of atoms, such as silicon (Si), germanium (Ge), and tin (Sn) in column IV and selenium (Se) and tellurium (Te) in column VI of the periodic table.',
-        to_timestamp(1613323407),
+        to_timestamp(1607958000),
         'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108731/samples/elephants.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
