40bb9e013bcdcab50544a95a861e4c660bf357b0
Mitra Nami <a.mxnami@gmail.com>
2020-11-23 19:10:53 -0500

    Add payment form

--- client/src/components/SubscriptionModal.js
+++ client/src/components/SubscriptionModal.js
@@ -9,7 +9,10 @@ Modal.setAppElement('#root');
 const SubscriptionModal = (props) => {
 
   const [state, setState] = useState({
-    name: ''
+    name: '',
+    cardNumber: '',
+    exp: '',
+    cvc: ''
   });
 
   const {
@@ -29,28 +32,62 @@ const SubscriptionModal = (props) => {
       .then(result => {
         const newSubscription = result.data;
         setSubscription([...subscriptions, newSubscription]);
+        setState(prev => ({name: '', cardNumber: '', exp: '', cvc: ''}));
         setModalIsOpen(false);
       })
       .catch(error => console.log(error, 'did not subscribe successfully'));
   };
 
   const handleChange = (evt) => {
+    setState(prev => ({...prev, [evt.target.name]: evt.target.value }));
+  }; 
 
-  } 
+  const handleCancel = () => {
+    setState(prev => ({name: '', cardNumber: '', exp: '', cvc: ''}));
+    setModalIsOpen(false);
+  };
 
   return(
     <Modal
       isOpen={modalIsOpen}
       onRequestClose={() => setModalIsOpen(false)}
+      style={
+        {
+          overlay: {
+            backgroundColor: '#D3D3D3E6'
+          },
+          content: {
+            color: 'black',
+            fontWeight: 'bold',
+            left: '35%',
+            width: '30%',
+            height: '50%',
+            backgroundColor: '#ff99cc',
+            border: 'black solid 3px',
+            borderRadius: '15px 50px 30px'
+          }
+        }
+      }
     >
-      <form onSubmit={handleSubscription}>
-        <label htmlFor="name">Name</label>
-        <input id="name" type="text" value={state.name} onChange={handleChange} required/>
+      <div className="container">
+        <form onSubmit={handleSubscription}>
+          <div className="form-group">
+            <label htmlFor="name">Name</label>
+            <input id="name" className="form-control" type="text" name="name" value={state.name} onChange={handleChange} required/>
+          </div>
+          <div className="form-group">
+            <label htmlFor="name">Card number</label>
+            <input id="cardNumber" className="form-control" type="text" name="cardNumber" value={state.cardNumber} onChange={handleChange} required/>
+          </div>
+          <div className="d-flex flex-row form-group">
+            <input id="exp" className="form-control" type="text" name="exp" placeholder="YY/MM" value={state.exp} onChange={handleChange} required/>
+            <input id="cvc" className="form-control" type="text" name="cvc" placeholder="CVC" value={state.cvc} onChange={handleChange} required/>
+          </div>
+          <button type="submit">Subscribe</button>
+        </form>
 
-        <button type="submit" onClick={handleSubscription}>Subscribe</button>
-      </form>
-
-      <button className="btn btn-outline-dark btn-lg" type="submit" onClick={() => setModalIsOpen(false)}>Cancel</button>
+        <button className="btn btn-outline-dark btn-lg" type="submit" onClick={handleCancel}>Cancel</button>
+      </div>
     </Modal>
   );
 
