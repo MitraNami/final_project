007e5b838c8fabab6b4292f2e9f5d066b83a40a7
Mitra Nami <a.mxnami@gmail.com>
2020-11-23 22:52:13 -0500

    Create routes for MyAccount

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -1,21 +1,47 @@
 
-import {Link} from 'react-router-dom';
+import {
+  Link,
+  useRouteMatch,
+  Switch,
+  Route
+} from 'react-router-dom';
 
 
 const MyAccount = () => {
 
+  const {path, url} = useRouteMatch();
 
   return(
     <div>
       <aside>
         <ul>
-          <li>Edit profile</li>
-          <li>Manage Subscription</li>
-          <li>My invoices</li>
-          <li>Enrolled courses</li>
+          <li>
+            <Link to={`${url}/profile`}>Edit profile</Link>
+          </li>
+          <li>
+            <Link to={`${url}/courses`}>Enrolled courses</Link>
+          </li>
+          <li>
+            <Link to={`${url}/subscriptions`}>Manage Subscription</Link>
+          </li>
+          <li>
+            <Link to={`${url}/invoices`}>My invoices</Link>
+          </li>
         </ul>
       </aside>
 
+      <Switch>
+        <Route exact path={path}>
+          <h3>Welcome to your account!!</h3>
+        </Route>
+        <Route exact path={`${path}/courses`}>
+          <h3>A list of enrolled courses</h3>
+        </Route>
+        <Route exact path={`${path}/subscriptions`}>
+          <h3>A list of subscriptions</h3>
+        </Route>
+
+      </Switch>
     </div>
   );
 
