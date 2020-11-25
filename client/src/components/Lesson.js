60e7981d6414f9a10e47606b4d8158471352ce2e
Danielle Bentivoglio Colturato <dbcolturato@gmail.com>
2020-11-25 14:45:37 -0500

    Confirm lesson deletion

--- client/src/components/Lesson.js
+++ client/src/components/Lesson.js
@@ -1,10 +1,11 @@
-import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { Link, useRouteMatch } from 'react-router-dom';
+import ConfirmDeleteModal from 'components/ConfirmDeleteModal';
 
 export default function Lesson(props) {
 
   const { url } = useRouteMatch();
+  const [modalShow, setModalShow] = useState(false);
 
   return (
     <div className="row">
@@ -12,11 +13,16 @@ export default function Lesson(props) {
         {props.title}
       </div>
       { props.admin && (<div className="col-1"><Link className="btn btn-primary" to={`${url}/lesson/${props.id}`}>Edit</Link></div>)}
-      { props.admin && (<div className="col-1"><button type="submit" className="btn btn-primary" onClick={del}>Delete</button></div>)}
+      { props.admin && (<div className="col-1"><button type="submit" className="btn btn-primary" onClick={handleDeleteClick}>Delete</button></div>)}
+      <ConfirmDeleteModal modalIsOpen={modalShow} setModalIsOpen={setModalShow} onDelete={deleteLesson} />
     </div>
   );
 
-  function del() {
+  function handleDeleteClick() {
+    setModalShow(true);
+  }
+
+  function deleteLesson() {
     props.deleteLesson(props.id);
   }
 }
