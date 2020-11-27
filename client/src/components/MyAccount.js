2fdd06d6b6c0b6d66c72ca1612b6d591ad5de60f
Mitra Nami <a.mxnami@gmail.com>
2020-11-27 15:54:26 -0500

    Create a form so that the user can change their profile info

--- client/src/components/MyAccount.js
+++ client/src/components/MyAccount.js
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom';
 
 import UserCourses from 'components/UserCourses';
+import EditProfile from 'components/EditProfile';
 
 
 const MyAccount = (props) => {
 
   const courses = props.state.courses;
   const userId = props.state.token.userId;
+  const users = props.state.users;
 
   const {path, url} = useRouteMatch();
 
@@ -21,7 +23,7 @@ const MyAccount = (props) => {
       <div  className="container">
         <ul className="row bg-light" style={{listStyleType: 'none'}}>
           <li className="col-sm">
-            <Link to={`${url}/profile`}>Edit profile</Link>
+            <Link to={`${url}/profile`}>My profile</Link>
           </li>
           <li className="col-sm">
             <Link to={`${url}/courses`}>Enrolled courses</Link>
@@ -39,7 +41,9 @@ const MyAccount = (props) => {
         <Route exact path={`${path}/courses`}>
           <UserCourses userId={userId} courses={courses} />
         </Route>
-
+        <Route>
+          <EditProfile userId={userId} users={users} />
+        </Route>
       </Switch>
     </div>
   );
