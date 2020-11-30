4a85f8a843599f9a526044c28d6ab59d03535951
Mitra Nami <a.mxnami@gmail.com>
2020-11-29 19:26:14 -0500

    Add logo component with its style

---
+++ client/src/components/Logo.js
@@ -0,0 +1,18 @@
+import '../style/logo.css'
+
+const Logo = () => {
+
+  return (
+    
+    <img 
+      className="logo" 
+      src="https://res.cloudinary.com/dxc1pdflu/image/upload/v1606690147/samples/strongerU_logo_k7py6x.png" 
+      alt="logo" 
+    />
+  );
+
+};
+
+
+
+export default Logo;
