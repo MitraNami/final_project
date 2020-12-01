a08118988ec9a917c5b966662eb7ba15f4c1732c
Mitra Nami <a.mxnami@gmail.com>
2020-11-30 23:31:58 -0500

    Add seeds

--- express_backend/db/seeds/02_courses.sql
+++ express_backend/db/seeds/02_courses.sql
@@ -2,30 +2,47 @@ INSERT INTO
 courses(title, description, subscription_based, price, authorized)
 VALUES
     (
-        'Learn more about the StrongerU Senior Fitness Instructor Certification and Series Free',
+        'Senior Fitness Instructor Certification',
         'Enroll in this free course to learn more about the StrongerU Senior Fitness Instructor Certification and Series. We will review the steps to becoming a StrongerU Senior Fitness Instructor, show you a preview of the choreography notes and instructional videos for the four StrongerU Senior Fitness Series formats- Cardio, Strength, Stretch, and Circuit, and more.',
         FALSE,
-        0,
+        25000,
         TRUE
     ),
     (
-        'StrongerU Senior Fitness Instructor Certification',
-        'The StrongerU Senior Fitness Instructor Certification is a premier first pre-choreographed senior fitness certification. Instructors complete this course at their own pace and from any device including a smartphone. At the end of the course, you will receive your first 30-minutes of choreography via an instructional video and choreography notes. The final step to becoming a fully certified StrongerU Senior Fitness Instructor is to submit a practical video of you teaching the choreography and showing you meet the program s core competencies. Upon receiving a passing grade on your practical video, you are now eligible to subscribe to the StrongerU Senior Fitness Series where each month you will receive 30-minutes of content in a rotation of Cardio, Strength, Stretch and Circuit. Please read our terms of services before completing your registration. By registering, you are accepting these terms of service.',
-        FALSE,
-        255000,
-        TRUE
-    ),
-    (
-        'StrongerU Senior Fitness Series',
+        'Senior Fitness',
         'Subscription to the StrongerU Senior Fitness Series provides StrongerU Senior Fitness Instructors with their licence to teach the StrongerU Senior Fitness program. Each month you will receive 30-minutes of new content, including; an instructional video and choreography notes.',
         TRUE,
-        100000,
+        0,
         FALSE
     ),
     (
-        'solid state physics',
-        'Introduction to semiconductors',
+        'Yoga',
+        'Workout fads come and go, but virtually no other exercise program is as enduring as yoga. Its been around for more than 5,000 years.
+        Yoga does more than burn calories and tone muscles. Its a total mind-body workout that combines strengthening and stretching poses with deep breathing and meditation or relaxation.
+        There are more than 100 different forms of yoga. Some are fast-paced and intense. Others are gentle and relaxing. Yes. Yoga poses stretch your muscles and increase your range of motion. With regular practice, theyll improve your flexibility.
+        Yes. It takes a lot of strength to hold your body in a balanced pose. Regular practice will strengthen the muscles of your arms, back, legs, and core.Do you have arthritis? Yoga can help you stay flexible and strong without putting added stress on your joints. You get the added benefit of a mind-body approach that can help you relax and energize.
+        Workout fads come and go, but virtually no other exercise program is as enduring as yoga. Its been around for more than 5,000 years. Yoga does more than burn calories and tone muscles. Its a total mind-body workout that combines strengthening and stretching poses with deep breathing and meditation or relaxation. There are more than 100 different forms of yoga. Some are fast-paced and intense. Others are gentle and relaxing. Yes. Yoga poses stretch your muscles and increase your range of motion. With regular practice, theyll improve your flexibility. Yes. It takes a lot of strength to hold your body in a balanced pose. Regular practice will strengthen the muscles of your arms, back, legs, and core.Do you have arthritis? Yoga can help you stay flexible and strong without putting added stress on your joints. You get the added benefit of a mind-body approach that can help you relax and energize.',
         TRUE,
         0,
+        TRUE
+    ),
+    (
+        'Stretch',
+        'Workout fads come and go, but virtually no other exercise program is as enduring as yoga. Its been around for more than 5,000 years.
+        Yoga does more than burn calories and tone muscles. Its a total mind-body workout that combines strengthening and stretching poses with deep breathing and meditation or relaxation.
+        There are more than 100 different forms of yoga. Some are fast-paced and intense. Others are gentle and relaxing. Yes. Yoga poses stretch your muscles and increase your range of motion. With regular practice, theyll improve your flexibility.
+        Yes. It takes a lot of strength to hold your body in a balanced pose. Regular practice will strengthen the muscles of your arms, back, legs, and core.Do you have arthritis? Yoga can help you stay flexible and strong without putting added stress on your joints. You get the added benefit of a mind-body approach that can help you relax and energize.',
+        FALSE,
+        15000,
+        TRUE
+    ),
+    (
+        'Aerobic',
+        'Workout fads come and go, but virtually no other exercise program is as enduring as yoga. Its been around for more than 5,000 years.
+        Yoga does more than burn calories and tone muscles. Its a total mind-body workout that combines strengthening and stretching poses with deep breathing and meditation or relaxation.
+        There are more than 100 different forms of yoga. Some are fast-paced and intense. Others are gentle and relaxing. Yes. Yoga poses stretch your muscles and increase your range of motion. With regular practice, theyll improve your flexibility.
+        Yes. It takes a lot of strength to hold your body in a balanced pose. Regular practice will strengthen the muscles of your arms, back, legs, and core.Do you have arthritis? Yoga can help you stay flexible and strong without putting added stress on your joints. You get the added benefit of a mind-body approach that can help you relax and energize.',
+        FALSE,
+        20000,
         TRUE
     );
--- express_backend/db/seeds/03_lessons.sql
+++ express_backend/db/seeds/03_lessons.sql
@@ -5,7 +5,7 @@ VALUES
         'Lesson 1',
         'Enroll in this free course to learn more about the StrongerU Senior Fitness Instructor Certification and Series. We will review the steps to becoming a StrongerU Senior Fitness Instructor, show you a preview of the choreography notes and instructional videos for the four StrongerU Senior Fitness Series formats- Cardio, Strength, Stretch, and Circuit, and more.',
         to_timestamp(1605575289),
-        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108731/samples/elephants.mp4',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         234,
         1
@@ -14,7 +14,7 @@ VALUES
         'Lesson 2',
         'The StrongerU Senior Fitness Instructor Certification is a premier first pre-choreographed senior fitness certification. Instructors complete this course at their own pace and from any device including a smartphone. At the end of the course, you will receive your first 30-minutes of choreography via an instructional video and choreography notes. The final step to becoming a fully certified StrongerU Senior Fitness Instructor is to submit a practical video of you teaching the choreography and showing you meet the program s core competencies. Upon receiving a passing grade on your practical video, you are now eligible to subscribe to the StrongerU Senior Fitness Series where each month you will receive 30-minutes of content in a rotation of Cardio, Strength, Stretch and Circuit. Please read our terms of services before completing your registration. By registering, you are accepting these terms of service.',
         to_timestamp(1552572000),
-        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         345,
         1
@@ -23,44 +23,53 @@ VALUES
         'Lesson 3',
         'Subscription to the StrongerU Senior Fitness Series provides StrongerU Senior Fitness Instructors with their licence to teach the StrongerU Senior Fitness program. Each month you will receive 30-minutes of new content, including; an instructional video and choreography notes.',
         to_timestamp(1550153400),
-        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         877,
         1
     ),
     (
-        'Lesson 1: what is a semiconductor? ',
-        'semiconductor lies between metal (conductors) and non-metal (insulators) therefore it behaves like an insulator and doesnt conduct electricity but depending upon the situation it can also conduct electricity and behave like an insulator.',
+        'Lesson 1: what is yoga? ',
+        'Yoga does more than burn calories and tone muscles.Compound exercises, which involve multiple joints and more than one muscle group. Its a total mind-body workout that combines strengthening and stretching poses with deep breathing and meditation or relaxation.',
         to_timestamp(1550153400),
-        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
-        4
+        3
     ),
     (
-        'Lesson 2: examples of semiconductors ',
-        'The study of semiconductor materials began in the early 19th century. The elemental semiconductors are those composed of single species of atoms, such as silicon (Si), germanium (Ge), and tin (Sn) in column IV and selenium (Se) and tellurium (Te) in column VI of the periodic table.',
+        'Lesson 2: Hatha',
+        'The form most often associated with yoga, it combines a series of basic movements with breathing. Compound exercises, which involve multiple joints and more than one muscle group. Compound exercises, which involve multiple joints and more than one muscle group.',
         to_timestamp(1552572000),
-        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
-        4
+        3
     ),
      (
-        'Lesson 3: semiconductors in industry',
-        'The study of semiconductor materials began in the early 19th century. The elemental semiconductors are those composed of single species of atoms, such as silicon (Si), germanium (Ge), and tin (Sn) in column IV and selenium (Se) and tellurium (Te) in column VI of the periodic table.',
+        'Lesson 3: Vinyasa',
+        'A series of poses that flow smoothly into one another. Compound exercises, which involve multiple joints and more than one muscle group. Compound exercises, which involve multiple joints and more than one muscle group.',
         to_timestamp(1555250400),
-        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108727/samples/sea-turtle.mp4',
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
-        4
+        3
     ),
     (
-        'Lesson 4: semiconductors in industry',
-        'The study of semiconductor materials began in the early 19th century. The elemental semiconductors are those composed of single species of atoms, such as silicon (Si), germanium (Ge), and tin (Sn) in column IV and selenium (Se) and tellurium (Te) in column VI of the periodic table.',
-        to_timestamp(1607958000),
-        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1605108731/samples/elephants.mp4',
+        'Lesson 4: Flexibility',
+        'Yoga poses stretch your muscles and increase your range of motion.Compound exercises, which involve multiple joints and more than one muscle group.Compound exercises, which involve multiple joints and more than one muscle group.',
+        to_timestamp(1606796226),
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
         'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
         400,
-        4
+        3
+    ),
+    (
+        'Lesson 5: Ashtanga',
+        'A series of poses, combined with a special breathing technique.Compound exercises, which involve multiple joints and more than one muscle group.',
+        to_timestamp(1607958000),
+        'https://res.cloudinary.com/dxc1pdflu/video/upload/v1606770960/samples/1911928363.mp4_gmnqrh.mp4',
+        'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf',
+        400,
+        3
     )
